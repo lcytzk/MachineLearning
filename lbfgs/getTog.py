cf2726524e04d67c42dd6eef05ee23c306ce707f
lcytzk <lcytzk@github.com>
2016-08-29 10:52:59 +0800

    add two file for vw auc

---
+++ lbfgs/getTog.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import sys
+
+def getT(filename):
+    fl = open('label.txt', 'r')
+    fp = open(filename, 'r')
+    for line in fl:
+        line = line.strip()
+        line2 = fp.readline().strip()
+        print '%s,%s' % (line, line2)
+
+
+if __name__ == "__main__":
+    filename = sys.argv[1]
+    getT(filename)
